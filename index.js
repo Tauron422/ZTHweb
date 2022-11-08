a304a7cdbe2371b0ada4bde933892c78ec9389ac
Zethereum ZTH <91985907+RoyalRanger@users.noreply.github.com>
2022-11-08 17:30:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Config = {
     defaultServer: 'https://europe.signum.network',
     SmartContractId: 10252065254978029504n,
     SmartContractRS: "",
-    authorisedCodeHash: 738377637144987047n,
+    authorisedCodeHash: 7113358627428169487n,
     assetId: "9518219425200752102",
     serverAlternatives: [
         "https://brazil.signum.network",
