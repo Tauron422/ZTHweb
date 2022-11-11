c4a933293b81ea2d899cd65484eea27e4e76f985
Zethereum ZTH <91985907+RoyalRanger@users.noreply.github.com>
2022-11-11 16:26:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,85 @@
 "use strict";
 
+import {createDeeplink}  './createDeeplink';
+
+ payload = {
+    "recipient": "S-9K9L-4CB5-88Y5-F5G4Z",
+    "amountPlanck": 10000000,
+    "feePlanck": 735000,
+    "message": "Hi, from a deep link",
+    "messageIsText": true,
+    "immutable": true,
+    "deadline": 24,
+    "encrypt": false
+}
+
+const deeplink = createDeeplink({
+    action: 'pay',
+    payload
+})
+
+window.addEventListener('wallet-event', (event) => {
+    const {payload, action} = event.detail
+
+    if (action === 'connected') {
+      document.querySelector('#account-connection span').innerText = payload.address
+      const avatar = document.querySelector('#account-connection img')
+      avatar.src = window.hashicon(payload.accountId, 64).toDataURL()
+      document.getElementById('successful-connection').classList.remove("is-hidden")
+      document.getElementById('network').innerText = payload.host;
+    }
+
+    if (action === 'disconnected') {
+      document.getElementById('successful-connection').classList.add("is-hidden")
+      document.getElementById('connect-button-text').innerText = 'Connect Wallet'
+      document.querySelector('#connect-button-icon span').classList.remove('is-hidden');
+      const avatar = document.querySelector('#connect-button-icon img')
+      avatar.src = ""
+      avatar.classList.add('is-hidden');
+    }
+
+    if (action === 'accountChanged') {
+      document.querySelector('#account-connection span').innerText = payload.address
+      const avatar = document.querySelector('#account-connection img')
+      avatar.src = window.hashicon(payload.accountId, 64).toDataURL()
+    }
+
+    if (action === 'networkChanged') {
+      document.getElementById('network').innerText = payload.nodeHost;
+    }
+
+  })
+}
+
+async function sendTestMessage() {
+  try {
+
+    if (!window.signumLedger) {
+      throw new Error("Ledger Client not initialized");
+    }
+
+    const link = document.getElementById('transaction-link')
+    link.classList.add('is-hidden')
+    const {unsignedTransactionBytes} = await window.signumLedger.message.sendMessage({
+      senderPublicKey: walletConnection.publicKey,
+      recipient: walletConnection.accountId, // send to self
+      message: "If you can read this message, then you successfully sent the message with XT Vanilla Demo App",
+      messageIsText: true,
+      feePlanck: sig$util.Amount.fromSigna('0.01').getPlanck()
+    })
+    const tx = await window.wallet.confirm(unsignedTransactionBytes)
+    link.classList.remove('is-hidden')
+    link.setAttribute('href', `https://t-chain.signum.network/tx/${tx.transactionId}`)
+  } catch (e) {
+    alert(e.message)
+  }
+}
+
+let bottom_oplata = document.getElementById('transaction-link')
+bottom_oplata.onclick = function() {
+  payload.amountPlanck = bottom_oplata.value;
+};
+
 const Config = {
     defaultServer: 'https://europe.signum.network',
     SmartContractId: 738377637144987047n,
